1c40edb2b7ba9025964967bb7da961599e097bdd
Siyu Duan <siyu.duan@harperjames.co.uk>
2024-05-19 20:16:45 +0100

    nav function working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'course-project';
+
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
