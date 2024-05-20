bfcffca7d04fd0ee899e148a5aa4fe3fa43b8138
Siyu Duan <siyu.duan@harperjames.co.uk>
2024-05-20 10:22:16 +0100

    passing data to recipe item component

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-item.component.css'
 })
 export class RecipeItemComponent {
-
+  @Input() recipe: Recipe;
 }
