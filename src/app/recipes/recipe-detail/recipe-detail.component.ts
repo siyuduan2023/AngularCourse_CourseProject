cf66f9bbe3f04c36d6a974081d8e94e5aa5a96e8
Siyu Duan <siyu.duan@harperjames.co.uk>
2024-05-20 10:46:32 +0100

    passing data to recipe-detail

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-detail.component.css'
 })
 export class RecipeDetailComponent {
-
+  @Input() recipe: Recipe;
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,8 +8,14 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Mapo Tofu', 'Mapo Tofu (麻婆豆腐 Mápó dòufǔ) is a famous Sichuan dish known for its spicy, numbing flavors. It features soft tofu and minced pork in a sauce made from fermented black beans, chili paste, and Sichuan peppercorns. Often served over rice, this dish offers a bold and satisfying taste, making it a beloved staple in Chinese cuisine.', 'https://bing.com/th?id=OSK.4f6a7a199cc2dd73a5cb974dec7071e5'),
     new Recipe('Egg Fried Rice', 'Fried rice (炒饭 chǎofàn) is a dish made from fried cooked rice and other ingredients, often including eggs, vegetables, seafood, or meat. Fried rice is one of the most common Chinese foods. It is easy to make fried rice at home using leftover rice and other meat or vegetables from the last meal.', 'https://media.citizen.co.za/wp-content/uploads/2023/08/Homemade-Egg-Rice.jpg')
   ];
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
