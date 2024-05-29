eb65a284bf2323fdffe5a69638807552674befc2
Siyu Duan <siyu.duan@harperjames.co.uk>
2024-05-29 16:03:51 +0100

    observables completed

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,9 @@
-import { EventEmitter } from "@angular/core";
+
 import { Ingredient } from "../shared/ingredient.model";
+import { Subject } from "rxjs";
 
 export class ShoppingListService {
-    ingredientsChanged = new EventEmitter<Ingredient[]>();
+    ingredientsChanged = new Subject<Ingredient[]>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Tofu', 1),
@@ -15,7 +16,7 @@ export class ShoppingListService {
 
       addIngredient(ingredient: Ingredient) {
         this.ingredients.push(ingredient);
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 
       addIngredients(ingredients: Ingredient[]) {
@@ -23,6 +24,6 @@ export class ShoppingListService {
         //   this.addIngredient(ingredient);
         // }
         this.ingredients.push(...ingredients); //use spread operator to add all ingredients at once
-        this.ingredientsChanged.emit(this.ingredients.slice());
+        this.ingredientsChanged.next(this.ingredients.slice());
       }
 }
